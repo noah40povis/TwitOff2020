5b8eebd5786510cb3957a1d9c31eb7c2a770fb8d
noah40povis <n.c.povis@gmail.com>
2020-07-19 22:29:37 -0400

    .

--- webb_app/__init__.py
+++ webb_app/__init__.py
@@ -14,7 +14,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-    #app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
     db.init_app(app)
     migrate.init_app(app, db)
 
